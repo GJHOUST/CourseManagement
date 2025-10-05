cd6ea23a9f4039a52fb359c8485c2e7cdb408f7f
GJHOUST <mariushorvat7@gmail.com>
2025-10-05 19:15:35 +0200

    fix

--- src/main/java/com/example/CourseManagement/controller/CourseController.java
+++ src/main/java/com/example/CourseManagement/controller/CourseController.java
@@ -1,7 +1,7 @@
 package com.example.CourseManagement.controller;
 import com.example.CourseManagement.models.dto.CourseCreateDTO;
 import com.example.CourseManagement.models.dto.CourseViewDTO;
-import com.example.CourseManagement.models.services.CourseServiceImp;
+import com.example.CourseManagement.models.services.CourseService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,35 +14,35 @@ import java.util.List;
 public class CourseController {
 
     @Autowired
-    private CourseServiceImp courseServiceImp;
+    private CourseService courseService;
 
 
     @PostMapping("/create")
     public ResponseEntity<CourseViewDTO> create(@Valid @RequestBody CourseCreateDTO dto){
-        CourseViewDTO savedCourse = courseServiceImp.createCourse(dto);
+        CourseViewDTO savedCourse = courseService.createCourse(dto);
         return ResponseEntity.ok(savedCourse);
     }
 
     @GetMapping("/getAllCourses")
     public ResponseEntity<List<CourseViewDTO>> getAllCourses(){
-        return ResponseEntity.ok(courseServiceImp.getAllCourses());
+        return ResponseEntity.ok(courseService.getAllCourses());
     }
 
     @GetMapping("/getCourse/{courseId}")
     public ResponseEntity<CourseViewDTO> getCourseById(@PathVariable Long courseId){
-        return ResponseEntity.ok(courseServiceImp.getCourseById(courseId));
+        return ResponseEntity.ok(courseService.getCourseById(courseId));
     }
 
     @PutMapping("/update/{courseId}")
     public ResponseEntity<CourseViewDTO> updateCourse(
             @PathVariable Long courseId,
             @RequestBody CourseCreateDTO dto) {
-        return ResponseEntity.ok(courseServiceImp.updateCourse(courseId, dto));
+        return ResponseEntity.ok(courseService.updateCourse(courseId, dto));
     }
 
     @DeleteMapping("/deleteCourse/{courseId}")
     public ResponseEntity<Void> deleteCousreById(@PathVariable Long courseId){
-        courseServiceImp.deleteCourseById(courseId);
+        courseService.deleteCourseById(courseId);
         return ResponseEntity.noContent().build();
     }
 
--- src/main/java/com/example/CourseManagement/controller/UserController.java
+++ src/main/java/com/example/CourseManagement/controller/UserController.java
@@ -2,7 +2,7 @@ package com.example.CourseManagement.controller;
 import com.example.CourseManagement.models.dto.UserLoginDTO;
 import com.example.CourseManagement.models.dto.UserRegistrationDTO;
 import com.example.CourseManagement.models.dto.UserViewDTO;
-import com.example.CourseManagement.models.services.UserServiceImp;
+import com.example.CourseManagement.models.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,36 +16,36 @@ import java.util.List;
 public class UserController {
 
     @Autowired
-    private UserServiceImp userServiceImp;
+    private UserService userService;
 
     @PostMapping("/register")
     public ResponseEntity<UserViewDTO> register(@Valid @RequestBody UserRegistrationDTO dto) {
-        UserViewDTO savedUser = userServiceImp.register(dto);
+        UserViewDTO savedUser = userService.register(dto);
         return ResponseEntity.ok(savedUser);
     }
 
 
     @PostMapping("/login")
     public ResponseEntity<UserViewDTO> login(@Valid @RequestBody UserLoginDTO dto) {
-        UserViewDTO user = userServiceImp.login(dto);
+        UserViewDTO user = userService.login(dto);
         return ResponseEntity.ok(user);
     }
 
     @GetMapping("/getAllUsers")
     public ResponseEntity<List<UserViewDTO>> getAllUsers() {
-        return ResponseEntity.ok(userServiceImp.getAllUsers());
+        return ResponseEntity.ok(userService.getAllUsers());
     }
 
 
     @GetMapping("/getUser/{userId}")
     public ResponseEntity<UserViewDTO> getUserById(@PathVariable Long userId) {
-        return ResponseEntity.ok(userServiceImp.getUserById(userId));
+        return ResponseEntity.ok(userService.getUserById(userId));
     }
 
 
     @DeleteMapping("/deleteUser/{userId}")
     public ResponseEntity<Void> deleteUserById(@PathVariable Long userId) {
-        userServiceImp.deleteUserById(userId);
+        userService.deleteUserById(userId);
         return ResponseEntity.noContent().build();
     }
 
